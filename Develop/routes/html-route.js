4b543678175aa68eaafd4cc88260175f271647df
sharon <martinezsharonr@gmail.com>
2021-03-16 19:44:18 -0800

    added post

--- Develop/routes/html-route.js
+++ Develop/routes/html-route.js
@@ -1,10 +1,12 @@
+// path allows us to access and interact with the file system
 const path = require('path');
 
 module.exports = app => {
+  // when user hits home page send file home page
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/home.html'));
   });
-  
+  // when user hits /notes send file to notes page
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
