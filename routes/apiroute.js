561ee76689304d7a906714d737d49d905bbc3001
sharon <martinezsharonr@gmail.com>
2021-03-17 22:47:14 -0800

    changed file paths now notes save and post

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -7,31 +7,31 @@ const uuid = require('uuid');
 
 
 module.exports = (app) => {
-  //get request from db file
+  //get request from db file and send file
   app.get('/api/notes', (req, res) => {
-      //send file 
-      res.sendFile(path.join(__dirname, '../db/db.json'));
+      res.sendFile(path.join(__dirname, '/../db/db.json'));
     });
+
 
     //post request from db
   app.post('/api/notes', (req, res) => {
     //local variables 
-    const dataBase = JSON.parse(fs.readFileSync('../db/db.json'));
+    const dataBase = JSON.parse(fs.readFileSync('db/./db.json'));
     const newNote = req.body;
 
     dataBase.push(newNote);
     newNote.id = uuid.v4();
     
-    fs.writeFileSync('../db/db.json', JSON.stringify(dataBase));
+    fs.writeFileSync('db/./db.json', JSON.stringify(dataBase));
     res.json(dataBase);
   });
    
+
   app.delete('api/notes/id:', (req,res) => {
-    const dataBase = JSON.parse(fs.readFileSync('../db/db.json'));
+    const dataBase = JSON.parse(fs.readFileSync('db/./db.json'));
     const deleted = dataBase.filter((delNote) => delNote.id !== req.params.id);
 
-    fs.writeFileSync('../db/db.json', JSON.stringify(deleted));
-
+    fs.writeFileSync('db/./db.json', JSON.stringify(deleted));
     res.json(deleted);
   })  
 };
