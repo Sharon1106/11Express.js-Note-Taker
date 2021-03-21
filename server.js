af017f5393d169a542428f794e3b9b372c2ed9b0
Sharon Martinez <martinezsharonr@gmail.com>
2021-03-21 01:33:35 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 // dependencies
 const express = require('express');
 
-// express app is //listening on port 8080
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // MIDDLEWARE
- // express app data parsing
 app.use(express.urlencoded({ extended: true }));
-// parse application/express
 app.use(express.json());
-//like Wilson said:
- // we need: express.static('public'); to read from the folder public
 app.use(express.static("public"));
 
-
-// we require the 2 app function modules
+// we require 2 app function modules
 require('./routes/apiroute')(app);
 require('./routes/htmlroute')(app);
 
-// server starts to listen
+
 app.listen(PORT, function() {
-  //to know if it's running
+
   console.log("App is listening on port " + PORT);
 });
