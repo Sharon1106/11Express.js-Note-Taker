a5b2e5acf872402ffab778b7a436eb1b613774f7
Sharon Martinez <martinezsharonr@gmail.com>
2021-03-21 01:35:38 -0800

    Update htmlroute.js

--- routes/htmlroute.js
+++ routes/htmlroute.js
@@ -1,17 +1,17 @@
 const path = require('path');
 
 module.exports = app => {
-  // when user hits home page send path home page
+  // path home page
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/home.html'));
   });
 
-  // when user hits /notes send path notes page
+  // path notes page
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
-  // If no matching route is found or is left empty default to home
+  // efault to home
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/home.html'));
   });
