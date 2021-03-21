5871c1b24d6917b02ac681e53c5e7792dd8ad93a
Sharon Martinez <martinezsharonr@gmail.com>
2021-03-21 01:39:46 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // dependencies
 const express = require('express');
-
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -9,12 +8,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// we require 2 app function modules
+// require2 app function modules
 require('./routes/apiroute')(app);
 require('./routes/htmlroute')(app);
 
-
 app.listen(PORT, function() {
-
   console.log("App is listening on port " + PORT);
 });
